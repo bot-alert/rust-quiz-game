132ad9599c5cfb0ff4a010b3ac5805f36080107b
Ashish Khatiwada <ashishkahtiwada091@gmail.com>
2024-01-27 07:13:29 +0545

    optimized the code base

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,35 @@
 use std::io;
 
 fn main() {
-    let file = csv::ReaderBuilder::new()
+    let mut file = csv::ReaderBuilder::new()
         .has_headers(false)
-        .from_path("problem.csv");
+        .from_path("problem.csv")
+        .unwrap_or_else(|e| panic!("Cannot read the csv file"));
 
 
-    match file {
-        Ok(mut content) => {
-            let mut total_question: f32 = 0.0;
-            let mut correct_ans: f32 = 0.0;
+    let mut total_question: f32 = 0.0;
+    let mut correct_ans: f32 = 0.0;
 
-            content.records()
-                .filter_map(|record| record.ok())
-                .for_each(|record| {
-                    if let (Some(question), Some(answer)) = (record.get(0), record.get(1)) {
-                        let mut input = String::new();
-                        println!("What is the sum of {} ", question);
-                        loop {
-                            if let Ok(_) = io::stdin().read_line(&mut input) {
-                                break;
-                            }
-                            println!("Cannot read the input, please try again");
-                            _ = io::stdin()
-                        }
-                        if input.trim() == answer {
-                            correct_ans = correct_ans + 1.0;
-                        }
-                    }
-                    total_question = total_question + 1.0;
-                });
+    file.records()
+        .filter_map(|record| record.ok())
+        .for_each(|record| {
+            if let (Some(question), Some(answer)) = (record.get(0), record.get(1)) {
+                let mut input = String::new();
+                println!("What is the sum of {} ", question);
+
+                while io::stdin().read_line(&mut input).is_err() {
+                    println!("Cannot read the input, please try again");
+                }
+
+                if input.trim() == answer {
+                    correct_ans += 1.0;
+                }
+            }
+            total_question += 1.0;
+        });
 
 
-            println!("Total question {} Correct ans {}", total_question, correct_ans);
+    println!("Total question {} Correct ans {}", total_question, correct_ans);
 
-            println!("Your accuracy is {} %", (correct_ans / total_question) * 100.0)
-        }
-        Err(_) => {
-            println!("Cannot read the csv file");
-        }
-    }
+    println!("Your accuracy is {} %", (correct_ans / total_question) * 100.0)
 }
